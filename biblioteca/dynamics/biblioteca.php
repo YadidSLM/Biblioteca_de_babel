ae157a23ecfac21a82f079df32f72588f207267b
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-06 01:54:49 -0500

    2 modos completos, en progreso del último

--- biblioteca/dynamics/biblioteca.php
+++ biblioteca/dynamics/biblioteca.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Biblioteca</title>
 </head>
 <body>
     <?php
@@ -33,6 +33,29 @@
                                         $num = rand(97, 122);
                                         $char = chr($num);
                                         echo $char;
+                                    }
+                                    if($c == $um && $modo == "Normal")
+                                    {
+                                        echo '<strong style="font-size:20px"><i> '.$buscar.'</i></strong>';
+                                    }
+                                    else if($c == $um && $modo == "Orden")
+                                    {
+                                        /*$psIngresadas = explode(" ", $buscar);
+                                        $ascii = ord($psIngresadas[0]);
+                                        $ascii2 = ord($psIngresadas[1]);*/
+                                        $psIngresadas = explode(" ", $buscar);
+                                        sort($psIngresadas);
+                                        foreach($psIngresadas as $llave => $valor)
+                                        {
+                                            echo "<strong style='font-size:20px'><i> $valor</i></strong>";
+                                        }
+                                        //echo $psIngresadas;
+                                    }
+                                    else if($c == $um && $modo == "Palabras")
+                                    {
+                                        $psIngresadas = explode(" ", $buscar);
+                                        $cuenta = count($psIngresadas);
+                                        echo "<strong style='font-size:20px'><i>".$psIngresadas[$cuenta]."</i></strong>";
                                     }
                                     echo "  ";
                                 }    
