b4d55c6e286d8d65b3422b9d45ea36d5cfcc2621
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-05 22:02:24 -0500

    2 Ternarios

--- biblioteca/dynamics/biblioteca.php
+++ biblioteca/dynamics/biblioteca.php
@@ -1,3 +1,5 @@
 <?php
-    $
+    $buscar = (isset($_POST['buscar']) && $_POST != "")? $_POST['buscar']: 'No especifico';
+    $timezone = (isset($_POST['timezone']) && $_POST != "")? $_POST['timezone']: 'No especifico';
+    echo $buscar;
 ?>
