6f1f5c251370463d0e4b5936cc782d75146e869a
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-05 23:37:05 -0500

    Párrafo de entre 300 y 500 palabras con palabras aleatorias de entre 4 y 10 letras

--- biblioteca/dynamics/biblioteca.php
+++ biblioteca/dynamics/biblioteca.php
@@ -11,36 +11,33 @@
         $buscar = (isset($_POST['buscar']) && $_POST != "")? $_POST['buscar']: 'No especifico';
         $timezone = (isset($_POST['timezone']) && $_POST != "")? $_POST['timezone']: 'No especifico';
         $modo = (isset($_POST['modo']) && $_POST != "")? $_POST['modo']: 'No especifico';
-        $a = 0;
-        echo $buscar.'<br/>'. $timezone.'<br/>'. $modo. '<br/>';
-        $i = rand(4, 8);
-        while($a <= $i)
-        {
-            $num = rand(97, 122);
-            $char = chr($num);
-            echo $char;
-            $a++;
-        }
-                echo '<table>'
+        //$a = 0;
+        $i = rand(4, 10);
+        $um = rand(97, 122);
+        $palabras = rand(300, 500);
+                echo "<table border='1'cellpadding='25px' style='margin:50px'>";
                     echo '
                     <thead>
                         <tr>
-                            <th>Libro'. $num. ($num + 413).'</th>
+                            <th>Libro '. $um. ($um + 413).'</th>
                         </tr>
                     </thead>
                     ';
                     echo'
                     <tbody>
                         <tr>
-                            <td>'.
-                                while($a <= $i)
+                            <td>';
+                                for($c = 0; $c <= $palabras; $c++)
                                 {
-                                    $num = rand(97, 122);
-                                    $char = chr($num);
-                                    echo $char;
-                                    $a++;
-                                }
-                            .'</td>
+                                    for($a = 0; $a <= $i; $a++)
+                                    {
+                                        $num = rand(97, 122);
+                                        $char = chr($num);
+                                        echo $char;
+                                    }
+                                    echo "  ";
+                                }    
+                            echo'</td>
                         </tr>
                     </tbody>
                     ';
