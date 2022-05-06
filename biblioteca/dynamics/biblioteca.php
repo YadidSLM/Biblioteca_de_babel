782a5ebb9ea51f6e255fe67ed4bf7307664651c5
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-05 22:41:56 -0500

    Palabras aleatorias hechas, inetnto de tabla

--- biblioteca/dynamics/biblioteca.php
+++ biblioteca/dynamics/biblioteca.php
@@ -1,5 +1,50 @@
-<?php
-    $buscar = (isset($_POST['buscar']) && $_POST != "")? $_POST['buscar']: 'No especifico';
-    $timezone = (isset($_POST['timezone']) && $_POST != "")? $_POST['timezone']: 'No especifico';
-    echo $buscar;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $buscar = (isset($_POST['buscar']) && $_POST != "")? $_POST['buscar']: 'No especifico';
+        $timezone = (isset($_POST['timezone']) && $_POST != "")? $_POST['timezone']: 'No especifico';
+        $modo = (isset($_POST['modo']) && $_POST != "")? $_POST['modo']: 'No especifico';
+        $a = 0;
+        echo $buscar.'<br/>'. $timezone.'<br/>'. $modo. '<br/>';
+        $i = rand(4, 8);
+        while($a <= $i)
+        {
+            $num = rand(97, 122);
+            $char = chr($num);
+            echo $char;
+            $a++;
+        }
+                echo '<table>'
+                    echo '
+                    <thead>
+                        <tr>
+                            <th>Libro'. $num. ($num + 413).'</th>
+                        </tr>
+                    </thead>
+                    ';
+                    echo'
+                    <tbody>
+                        <tr>
+                            <td>'.
+                                while($a <= $i)
+                                {
+                                    $num = rand(97, 122);
+                                    $char = chr($num);
+                                    echo $char;
+                                    $a++;
+                                }
+                            .'</td>
+                        </tr>
+                    </tbody>
+                    ';
+                echo '</table>';
+    ?>
+</body>
+</html>
